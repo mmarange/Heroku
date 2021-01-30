dcdc35254dbac509b764a4cede945954f55f3452
Meakin <meakin.marange@gmail.com>
2021-01-30 17:06:24 -0500

    forward slash correction

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 @app.route("/samples")
 def samples():
 
-    samples_json = open("static\data\samples.json", "r") 
+    samples_json = open("static/data/samples.json", "r") 
     samples_data = json.load(samples_json)
     # open the json file, located at static/data/samples.json
     # use json.load() to read in the file as json
